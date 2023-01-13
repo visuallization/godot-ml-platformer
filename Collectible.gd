99f9f4b6691ebda9c549dcef9dd4979ee5c81645
visuallisation <florentin.rieger@gmail.com>
2023-01-14 00:16:09 +0100

    feat(collectible): make it destroyable on body enter

--- Collectible.gd
+++ Collectible.gd
@@ -1,7 +1,13 @@
-extends StaticBody
+extends Area
 
 export var rotation_speed := 1	
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotate(Vector3(0, 1, 0), rotation_speed * delta)
+
+func _on_body_entered(body:Node):
+	queue_free()
